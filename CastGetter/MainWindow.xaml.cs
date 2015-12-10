9a755d6d5012db4b23c704ebd72e0a9e51b04955
Michael Tissen <michael.tissen@gmail.com>
2015-12-11 00:47:47 +0100

    First UI Mockup created

---
+++ CastGetter/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CastGetterLib;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace CastGetter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Podcast> items = new List<Podcast>();
+            items.Add(new Podcast() { Name = "Celluleute", Description = "Ein Filmpodcast" });
+            lvPodcasts.ItemsSource = items;
+
+            List<Episode> episodeList = new List<Episode>();
+            episodeList.Add(new Episode() {Name = "Horrorfilme",
+                AlreadySync =false,
+                Date = DateTime.Now,
+                Description = "Es geht um die besten Horrorfilme",
+                Downloaded = false,
+                Duration = "2:05:00" });
+            episodeList.Add(new Episode()
+            {
+                Name = "Weihnachtsfilme",
+                AlreadySync = false,
+                Date = DateTime.Now,
+                Description = "Es geht um die besten Weihnachtsfilme",
+                Downloaded = false,
+                Duration = "2:20:00"
+            });
+            lvEpisodes.ItemsSource = episodeList;
+        }
+    }
+}
---
+++ CastGetterLib/Episode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastGetterLib
+{
+    public class Episode
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Duration { get; set; }
+        public bool AlreadySync { get; set; }
+        public bool Downloaded { get; set; }
+    }
+}
