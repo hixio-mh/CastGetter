ed0daafae17e924d6b85839aea8f9178ed765487
Michael Tissen <rubiktubik@gmail.com>
2016-01-06 01:51:13 +0100

    DownloadButton for every episode created, DownloadButton is recognized but not for special episode, serial download is working

--- CastGetter/ViewModels/PodcastViewModel.cs
+++ CastGetter/ViewModels/PodcastViewModel.cs
@@ -3,6 +3,9 @@ using CastGetter.Interface;
 using System.Collections.ObjectModel;
 using System;
 using System.Windows.Media;
+using System.Net;
+using System.IO;
+using System.Linq;
 
 namespace CastGetter.ViewModels
 {
@@ -14,16 +17,46 @@ namespace CastGetter.ViewModels
 
         private IEventAggregator _events;
 
+        private WebClient client;
+
         public PodcastViewModel(IEventAggregator events)
         {
             _podcasts = new ObservableCollection<Podcast>();
             //_episodes = new ObservableCollection<Episode>();
-
+            DownloadColor = new SolidColorBrush(Colors.Green);
             _events = events;
             _events.Subscribe(this);
         }
 
+        #region Commands
+
+        public void DownloadButton()
+        {
+            if (client == null)
+            {
+                client = new WebClient();
+            }
+            var hasExtension = Path.HasExtension(SelectedEpisode.LinkToRessource.AbsolutePath);
+            if (hasExtension)
+            {
+                string filePath = CleanFileName(SelectedPodcast.Name);
+                
+                client.DownloadFile(SelectedEpisode.LinkToRessource, filePath);
+            }
+        }
+
+        #endregion
+
         #region Properties
+
+        private SolidColorBrush _DownLoadColor;
+
+        public SolidColorBrush DownloadColor
+        {
+            get { return _DownLoadColor; }
+            set { _DownLoadColor = value; }
+        }
+
 
         private Podcast _SelectedPodcast;
             
@@ -36,6 +69,18 @@ namespace CastGetter.ViewModels
             }
         }
 
+        private Episode _SelectedEpisode;
+
+        public Episode SelectedEpisode
+        {
+            get { return _SelectedEpisode; }
+            set {
+                _SelectedEpisode = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+
 
         public ObservableCollection<Podcast> PodcastList
         {
@@ -47,6 +92,15 @@ namespace CastGetter.ViewModels
         {
             PodcastList.Add(message);
             SelectedPodcast = PodcastList[PodcastList.IndexOf(message)];
+        }
+
+        #endregion
+
+        #region Helper
+
+        private static string CleanFileName(string fileName)
+        {
+            return Path.GetInvalidFileNameChars().Aggregate(fileName, (current, c) => current.Replace(c.ToString(), string.Empty));
         }
 
         #endregion
